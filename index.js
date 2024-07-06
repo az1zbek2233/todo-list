0ab5428a439a69ecab48a87c72a34f90d096058a
Azizbek Rustamov <rystamov2233@gmail.com>
2024-07-06 14:59:49 +0500

    todo

---
+++ index.js
@@ -0,0 +1,17 @@
+let input = document.querySelector("input");
+let button = document.querySelector("button");
+let list = document.querySelector("ul");
+
+button.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    let task = input.value;
+    let li = document.createElement("li");
+
+    li.textContent = task;
+
+    list.appendChild(li);
+
+    input.value = "";
+
+})
