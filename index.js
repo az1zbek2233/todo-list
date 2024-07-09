3efec1b61888726493b032334ddd47859357bf5e
Azizbek Rustamov <rystamov2233@gmail.com>
2024-07-09 14:57:21 +0500

    done

--- index.js
+++ index.js
@@ -7,8 +7,30 @@ button.addEventListener("click", function (event) {
 
     let task = input.value;
     let li = document.createElement("li");
+    let span = document.createElement("span");
+    let doneBtn = document.createElement("button");
+    let delBtn = document.createElement("button");
 
-    li.textContent = task;
+    doneBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        span.classList.toggle("strike");
+    })
+
+    delBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        li.remove();
+    })
+
+    span.textContent = task;
+    doneBtn.textContent = "Done";
+    delBtn.textContent = "Delete";
+
+    li.appendChild(span);
+    li.appendChild(doneBtn);
+    li.appendChild(delBtn);
+    
 
     list.appendChild(li);
 
